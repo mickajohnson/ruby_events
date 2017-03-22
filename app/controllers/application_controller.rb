3b22e3a05db92c78b1e4f429e2b30cb296ce179c
Mick Johnson <mickajohnson@outlook.com>
2017-03-22 11:57:06 -0500

    Added user authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :require_login
+  def require_login
+    unless session[:user_id]
+      redirect_to '/sessions/new'
+    end
+  end
   def current_user
     User.find(session[:user_id]) if session[:user_id]
   end
